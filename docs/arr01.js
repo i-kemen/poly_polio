bf914a57a0dd7a5e9da9f5a11f39b94549bd4313
i-kemen <lynshu19@gmail.com>
2023-05-23 19:09:29 +0900

    5/23 로그인 페이지 작업

---
+++ docs/arr01.js
@@ -0,0 +1,7 @@
+let str = "광명폴리텍대학교";
+console.log(str);
+console.log("문자열의 길이는 " + str.length);
+
+for(let i=0; i < str.length; i ++){
+    console.log(str[i]);
+}
---
+++ src/back01.js
@@ -0,0 +1,11 @@
+name = "백두산";​
+
+classroom = 205;​
+
+console.log(`${name}님, ${classroom}호 강의실로 입장하세요.`);​
+
+console.log(name + '님, ' + classroom + '호 강의실로 입장하세요.');​
+
+let var1 = Symbol()​;
+
+let var2 = Symbor()​;
